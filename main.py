2ba6d2bfd695a7ba43edce973177ebd4723a7401
jSSjin <jerat004@gmail.com>
2023-10-15 23:07:27 +0700

    สร้าง class ไว้สำหรับการยิงเวท ปกติ

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from typing import Any
 import pygame, sys
+
+#from pygame.sprite import _Group
 from level101 import *
 from level import Level
+
+#normal projectile ---not skill---
+class magic_missle(pygame.sprite.Sprite):
+    def __init__(self, player_pos_x, player_pos_y, speed) :
+        super().__init__()
+        self.image = pygame.surface((20,10))
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect(center = (player_pos_x,player_pos_y))
+        self.speed = speed
+
+def create_magic_missle(x, y):
+    return magic_missle(x +1, y)
 
 def main():
     pygame.init()
@@ -19,6 +34,10 @@ def main():
     y = 250
     vel = 5
 
+    magic_missiles = pygame.sprite.Group()
+
+    #SPEED
+    speed = 10
     while run:
         pygame.time.delay(10)
         for event in pygame.event.get():
@@ -40,6 +59,7 @@ def main():
         pygame.draw.circle(win, (255, 0, 0), [x, y], width, 0)
 
         pygame.display.update()
+        magic_missle.update(x)
         clock.tick(60)
     pygame.quit()
 
