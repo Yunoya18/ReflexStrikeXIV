6fae7265884fc4eeecfc68665497e3fe0a4418af
Yunoya <sonrasamon15725@gmail.com>
2023-10-16 21:34:07 +0700

    ลบการเคลื่นไหวซ้ำที่หน้า main

--- main.py
+++ main.py
@@ -17,26 +17,6 @@ class magic_missle(pygame.sprite.Sprite):
 def create_magic_missle(x, y):
     return magic_missle(x +1, y)
 
-#toggle skill
-class toggle_skill(pygame.sprite.Sprite):
-    def __init__(self, player_x, player_y, win):
-        super().__init__()
-        self.font = pygame.font.Font(None, 32)
-        self.text = ""
-        self.input_box = pygame.Rect(player_x, player_y, 140, 32)
-        self.color = pygame.Color("lightskyblue3")
-        self.screen = win
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                elif event.key != pygame.K_SPACE:
-                    self.text += event.unicode
-        pygame.draw.rect(self.screen, self.color, self.input_box)
-        text_surface = self.font.render(self.text, True, (255, 255, 255))
-        self.screen.blit(text_surface, (self.input_box.x+5, self.input_box.y+5))
-        self.input_box.w = text_surface.get_width()+10
-
 def main():
     pygame.init()
 
@@ -63,16 +43,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and x > width:
-            x -= vel
-        if keys[pygame.K_RIGHT] and x < 500 - width:
-            x += vel
-        if keys[pygame.K_UP] and y > width:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < 500 - width:
-            y += vel
 
         win.fill(('black'))
         level.run()
