b0be7483ff0ee2de1e455b45b2afcb6e21353d4e
jSSjin <jerat004@gmail.com>
2023-11-11 22:13:41 +0700

    update tile png

--- tiles.py
+++ tiles.py
@@ -3,8 +3,7 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size):
         super().__init__()
-        self.image = pygame.Surface((size,size))
-        self.image.fill('Grey')
+        self.image = pygame.image.load('tile/tile_1.png')
         self.rect = self.image.get_rect(topleft = pos)
     def update(self,x_shift):
         self.rect.x += x_shift
