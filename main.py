e44c8adf7454d42d4c9acb2ea1d90f52d4b60747
SasakiYumeko <66070241@kmitl.ac.th>
2023-11-14 04:10:37 +0700

    fix dead can run

--- main.py
+++ main.py
@@ -405,6 +405,7 @@ while run:
             screen.blit(score_text, (1000, 30))
             if health == 0:
                 dead = True
+                player.speed = 0
                 player.update_action(2)
                 dead_text = font.render("DEAD", True, (255, 0, 0))
                 restart_text = font.render("press \'r\' to restart", True, (255, 255, 255))
